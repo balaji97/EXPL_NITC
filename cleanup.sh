035607ad28da3b3b2c4c80da3e080394763edf85
J Balaji <balaji.jayasankar@gmail.com>
2018-01-27 11:56:18 +0000

    Cleaned up code

--- cleanup.sh
+++ cleanup.sh
@@ -2,3 +2,6 @@ rm y.tab.c
 rm y.tab.h
 rm a.out
 rm a.xsm
+rm lex.yy.c
+rm test.xsm
+rm test.exe
--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
-./cleanup.sh
-yacc -d test.y
-lex test.l
+
+yacc -d compiler_yacc.y
+lex compiler_lex.l
 gcc y.tab.c lex.yy.c -o test.exe
 ./test.exe input.expl
-lex label.l
+lex compiler_label.l
 gcc lex.yy.c
 ./a.out
 ./xsm -l library.lib -e a.xsm
+./cleanup.sh
